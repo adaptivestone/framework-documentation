a79334a69b5908b823ed64690c2e297c9831a8db
Andrey <systerr@gmail.com>
2021-10-31 22:25:21 +0800

    meta

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,7 +31,7 @@ export default function Home() {
   return (
     <Layout
       title={`${siteConfig.title} documentation`}
-      description="Description will go into a meta tag in <head />"
+      description="AdaptiveStone framework documentation"
     >
       <HomepageHeader />
       <main>
